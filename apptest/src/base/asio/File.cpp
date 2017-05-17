2111e294ff4fe16fc354d22b32608ac736e0f40c
Noam Raz <noamraz8@gmail.com>
2017-05-17 23:18:15 +0300

    Implement File::open

--- apptest/src/base/asio/File.cpp
+++ apptest/src/base/asio/File.cpp
@@ -1,4 +1,5 @@
 #include "File.h"
+#include "base/unicode.h"
 #include "base/win/lastError.h"
 #include <memory>
 #include <type_traits>
@@ -36,13 +37,35 @@ void IoCompleteCallback(DWORD err, DWORD bytesTransferred, OVERLAPPED* overlappe
 }  // namespace
 
 
-File::File(std::string_view name, Mode mode) {
+File::File(std::string_view name, Flags mode) {
 	open(name, mode);
 }
 
 
-void File::open(std::string_view name, Mode mode) {
+void File::open(std::string_view name, Flags flags) {
+	DWORD access = 0, createDisp = 0;
 
+	if (flags & Flags::openOnly) {
+		createDisp = OPEN_EXISTING;
+	} else if (flags & Flags::createOnly) {
+		createDisp = CREATE_NEW;
+	} else if (flags & Flags::openAlways) {
+		createDisp = OPEN_ALWAYS;
+	} else if (flags & Flags::createAlways) {
+		createDisp = CREATE_ALWAYS;
+	}
+
+	if (flags & Flags::in) {
+		access |= GENERIC_READ;
+	}
+	if (flags & Flags::out) {
+		access |= GENERIC_WRITE;
+	}
+
+	mHandle = ::CreateFileW(u8ToU16(name).c_str(), access, FILE_SHARE_READ, nullptr, createDisp, FILE_FLAG_OVERLAPPED, nullptr);
+	if (!mHandle) {
+		win::throwLastError("Failed to open file");
+	}
 }
 
 void File::close() {
--- apptest/src/base/asio/File.h
+++ apptest/src/base/asio/File.h
@@ -10,17 +10,23 @@ namespace base {
 
 class File {
 public:
-	enum Mode {
-		readMode,
-		writeMode
+	enum Flags {
+		openOnly = 1 << 0,  // open only if exists
+		createOnly = 1 << 1,  // create only if doesn't exist
+
+		openAlways = 1 << 2,  // open or create
+		createAlways = 1 << 3,  // open and trucate or create
+		
+		in = 1 << 4,  // read
+		out = 1 << 5  // write
 	};
 
 	using CompleteCallback = std::function<void(unsigned long, const std::error_code&)>;
 
 	File() = default;
-	File(std::string_view name, Mode mode);
+	File(std::string_view name, Flags flags);
 
-	void open(std::string_view name, Mode mode);
+	void open(std::string_view name, Flags flags);
 	void close();
 
 	void read(std::int64_t offset, void* buf, unsigned long count, CompleteCallback callback);
