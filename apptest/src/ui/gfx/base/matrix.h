b41c01577e585064e820b58d34f66b048647ccf7
Noam Raz <noamraz8@gmail.com>
2017-07-19 12:09:17 +0300

    Add gfx::matrix

---
+++ apptest/src/ui/gfx/base/matrix.h
@@ -0,0 +1,55 @@
+#pragma once
+
+namespace gfx {
+
+template<typename Rep, int M, int N>
+struct matrix {
+	Rep data[M * N];
+
+	constexpr Rep& get(int row, int col);
+	constexpr const Rep& get(int row, int col) const;
+};
+
+
+template<typename Rep, int M, int N>
+constexpr Rep& matrix<Rep, M, N>::get(int row, int col) {
+	return data[row * N + col];
+}
+
+template<typename Rep, int M, int N>
+constexpr const Rep& matrix<Rep, M, N>::get(int row, int col) const {
+	return data[row * N + col];
+}
+
+
+template<typename Rep, int M, int N, int P>
+constexpr matrix<Rep, M, P> operator*(const matrix<Rep, M, N>& lhs, const matrix<Rep, N, P>& rhs) {
+	matrix<Rep, M, P> ret{};
+
+	for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < P; ++j) {
+			int total = 0;
+			for (int k = 0; k < N; ++k) {
+				total += lhs.get(i, k) * rhs.get(k, j);
+			}
+			ret.get(i, j) = total;
+		}
+	}
+
+	return ret;
+}
+
+
+template<int M, int N>
+using matf = matrix<float, M, N>;
+
+template<int M, int N>
+using mati = matrix<int, M, N>;
+
+using mat32f = matf<3, 2>;
+using mat33f = matf<3, 3>;
+
+using mat32i = mati<3, 2>;
+using mat33i = mati<3, 3>;
+
+}  // namespace gfx
