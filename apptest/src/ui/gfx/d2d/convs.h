0dd6ff9b219c6268c0252faef4faccf3fa6d33de
Noam Raz <noamraz8@gmail.com>
2017-07-19 21:18:22 +0300

    Add Direct2D conversion utilities

---
+++ apptest/src/ui/gfx/d2d/convs.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "ui/gfx/base/color.h"
+#include "ui/gfx/base/matrix.h"
+#include "ui/gfx/base/point.h"
+#include "ui/gfx/base/rect.h"
+#include <cmath>
+#include <d2d1_1.h>
+
+
+namespace gfx::impl {
+
+constexpr D2D1_COLOR_F color_to_d2d_color(color col) {
+	return {
+		col.r / 255.f,
+		col.g / 255.f,
+		col.b / 255.f,
+		col.a / 255.f
+	};
+}
+
+
+constexpr D2D1_POINT_2F point_to_d2d_point(const pointf& pt) {
+	return { pt.x, pt.y };
+}
+
+constexpr pointf d2d_point_to_point(const D2D1_POINT_2F& pt) {
+	return { pt.x, pt.y };
+}
+
+
+constexpr D2D1_RECT_F rect_to_d2d_rect(const rectf& rc) {
+	return {
+		rc.x,
+		rc.y,
+		rc.x + rc.width,
+		rc.y + rc.height
+	};
+}
+
+constexpr rectf d2d_rect_to_rect(const D2D1_RECT_F& rc) {
+	return {
+		rc.top,
+		rc.left,
+		std::abs(rc.top - rc.bottom),
+		std::abs(rc.left - rc.right)
+	};
+}
+
+
+constexpr D2D1_MATRIX_3X2_F mat32_to_d2d_mat32(const mat32f& mat) {
+	return {
+		mat.get(0, 0), mat.get(0, 1),
+		mat.get(1, 0), mat.get(1, 1),
+		mat.get(2, 0), mat.get(2, 1)
+	};
+}
+
+constexpr mat32f d2d_mat32_to_mat32(const D2D1_MATRIX_3X2_F& mat) {
+	return {
+		mat._11, mat._12,
+		mat._21, mat._22,
+		mat._31, mat._32
+	};
+}
+
+}  // namespace gfx::impl
