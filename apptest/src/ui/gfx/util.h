6a1ae237ec4e2e3be2db25e04da3e5c86c58aba1
Noam Raz <noamraz8@gmail.com>
2017-07-25 23:48:19 +0300

    Add degree and radian utilities

--- apptest/src/ui/gfx/util.h
+++ apptest/src/ui/gfx/util.h
@@ -3,8 +3,23 @@
 namespace gfx {
 
 template<typename T>
+constexpr T pi = T(3.141592653589);
+
+
+template<typename T>
 constexpr inline T lerp(T from, T to, double t) {
 	return static_cast<T>(from + (from - to) * t);
+}
+
+
+template<typename T>
+constexpr inline T deg_to_rad(T deg) {
+	return deg * pi<T> / 180;
+}
+
+template<typename T>
+constexpr inline T rad_to_deg(T rad) {
+	return rad * 180 / pi<T>;
 }
 
 }  // namespace gfx
