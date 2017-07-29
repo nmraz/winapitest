0ce6f8a9a4b18e2ac57a7cda0f25cf8381b093ee
Noam Raz <noamraz8@gmail.com>
2017-07-29 19:32:34 +0300

    Constrain scoped_co_init constructors

--- apptest/src/base/win/scoped_co_init.h
+++ apptest/src/base/win/scoped_co_init.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "base/non_copyable.h"
+
 namespace base::win {
 
-class scoped_co_init {
+class scoped_co_init : public non_copy_movable {
 public:
-	scoped_co_init(bool mta = false);
+	explicit scoped_co_init(bool mta = false);
 	~scoped_co_init();
 };
 
