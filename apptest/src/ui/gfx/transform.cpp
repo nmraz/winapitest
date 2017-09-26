dfe5648ca3a0fcb5280d18554da00bd821b5a461
Noam Raz <noamraz8@gmail.com>
2017-09-26 22:09:52 +0300

    Constexpr-ize transform matrix generation

--- apptest/src/ui/gfx/transform.cpp
+++ apptest/src/ui/gfx/transform.cpp
@@ -4,30 +4,6 @@
 
 namespace gfx::transform {
 
-mat33f translate(float dx, float dy) {
-  return {
-    1,  0,  0,
-    0,  1,  0,
-    dx, dy, 1
-  };
-}
-
-mat33f scale(float sx, float sy) {
-  return {
-    sx, 0,  0,
-    0,  sy, 0,
-    0,  0,  1
-  };
-}
-
-mat33f shear(float shx, float shy) {
-  return {
-    1,   shy, 0,
-    shx, 1,   0,
-    0,   0,   1
-  };
-}
-
 mat33f rotate(float theta) {
   float sin_theta = std::sin(theta);
   float cos_theta = std::cos(theta);
@@ -36,21 +12,6 @@ mat33f rotate(float theta) {
     cos_theta,  sin_theta,  0,
     -sin_theta, cos_theta,  0,
     0,          0,          1
-  };
-}
-
-mat33f centered_about(const mat33f& tform, const pointf& center) {
-  // translate `center` to origin, apply transform, and translate back
-  return translate(-center.x, -center.y) * tform * translate(center.x, center.y);
-}
-
-
-pointf apply(const mat33f& tform, const pointf& pt) {
-  // postmultiply `pt` by `tform`
-
-  return {
-    pt.x * tform.get(0, 0) + pt.y * tform.get(1, 0) + tform.get(2, 0),
-    pt.x * tform.get(0, 1) + pt.y * tform.get(1, 1) + tform.get(2, 1)
   };
 }
 
--- apptest/src/ui/gfx/transform.h
+++ apptest/src/ui/gfx/transform.h
@@ -18,14 +18,45 @@ constexpr mat33f identity() {
   };
 }
 
-mat33f translate(float dx, float dy);
-mat33f scale(float sx, float sy);
-mat33f shear(float shx, float shy);
+constexpr mat33f translate(float dx, float dy) {
+  return {
+    1,  0,  0,
+    0,  1,  0,
+    dx, dy, 1
+  };
+}
+
+constexpr mat33f scale(float sx, float sy) {
+  return {
+    sx, 0,  0,
+    0,  sy, 0,
+    0,  0,  1
+  };
+}
+
+constexpr mat33f shear(float shx, float shy) {
+  return {
+    1,   shy, 0,
+    shx, 1,   0,
+    0,   0,   1
+  };
+}
+
 mat33f rotate(float theta);
 
-mat33f centered_about(const mat33f& tform, const pointf& center);
+constexpr mat33f centered_about(const mat33f& tform, const pointf& center) {
+  // translate `center` to origin, apply transform, and translate back
+  return translate(-center.x, -center.y) * tform * translate(center.x, center.y);
+}
 
 
-pointf apply(const mat33f& tform, const pointf& pt);
+constexpr pointf apply(const mat33f& tform, const pointf& pt) {
+  // postmultiply `pt` by `tform`
+
+  return {
+    pt.x * tform.get(0, 0) + pt.y * tform.get(1, 0) + tform.get(2, 0),
+    pt.x * tform.get(0, 1) + pt.y * tform.get(1, 1) + tform.get(2, 1)
+  };
+}
 
 }  // namespace gfx::transform
