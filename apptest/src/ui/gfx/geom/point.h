a86d85b912890a1c4b7d0c17fff2239df4a64b8c
Noam Raz <noamraz8@gmail.com>
2017-09-28 21:54:51 +0300

    Fix misnamed variables in point comparison

--- apptest/src/ui/gfx/geom/point.h
+++ apptest/src/ui/gfx/geom/point.h
@@ -43,28 +43,28 @@ void point<Rep>::set(Rep new_x, Rep new_y) {
 
 
 template<typename Rep>
-constexpr point<Rep>& operator+=(point<Rep>& rhs, const point<Rep>& lhs) {
-  rhs.x += lhs.x;
-  rhs.y += lhs.y;
-  return rhs;
+constexpr point<Rep>& operator+=(point<Rep>& lhs, const point<Rep>& rhs) {
+  lhs.x += rhs.x;
+  lhs.y += rhs.y;
+  return lhs;
 }
 
 template<typename Rep>
-constexpr point<Rep> operator+(point<Rep> rhs, const point<Rep>& lhs) {
-  return rhs += lhs;
+constexpr point<Rep> operator+(point<Rep> lhs, const point<Rep>& rhs) {
+  return lhs += rhs;
 }
 
 
 template<typename Rep>
-constexpr point<Rep>& operator-=(point<Rep>& rhs, const point<Rep>& lhs) {
-  rhs.x -= lhs.x;
-  rhs.y -= lhs.y;
-  return rhs;
+constexpr point<Rep>& operator-=(point<Rep>& lhs, const point<Rep>& rhs) {
+  lhs.x -= rhs.x;
+  lhs.y -= rhs.y;
+  return lhs;
 }
 
 template<typename Rep>
-constexpr point<Rep> operator-(point<Rep> rhs, const point<Rep>& lhs) {
-  return rhs -= lhs;
+constexpr point<Rep> operator-(point<Rep> lhs, const point<Rep>& rhs) {
+  return lhs -= rhs;
 }
 
 template<typename Rep>
@@ -74,33 +74,33 @@ constexpr point<Rep> operator-(const point<Rep>& pt) {
 
 
 template<typename Rep, typename Rep2>
-constexpr point<Rep>& operator*=(point<Rep>& rhs, Rep2 value) {
-  rhs.x *= value;
-  rhs.y *= value;
-  return rhs;
+constexpr point<Rep>& operator*=(point<Rep>& lhs, Rep2 value) {
+  lhs.x *= value;
+  lhs.y *= value;
+  return lhs;
 }
 
 template<typename Rep, typename Rep2>
-constexpr point<Rep> operator*(point<Rep> rhs, Rep2 value) {
-  return rhs *= value;
+constexpr point<Rep> operator*(point<Rep> lhs, Rep2 value) {
+  return lhs *= value;
 }
 
 template<typename Rep, typename Rep2>
-constexpr point<Rep> operator*(Rep2 value, const point<Rep>& lhs) {
-  return lhs * value;
+constexpr point<Rep> operator*(Rep2 value, const point<Rep>& rhs) {
+  return rhs * value;
 }
 
 
 template<typename Rep, typename Rep2>
-constexpr point<Rep>& operator/=(point<Rep>& rhs, Rep2 value) {
-  rhs.x /= value;
-  rhs.y /= value;
-  return rhs;
+constexpr point<Rep>& operator/=(point<Rep>& lhs, Rep2 value) {
+  lhs.x /= value;
+  lhs.y /= value;
+  return lhs;
 }
 
 template<typename Rep, typename Rep2>
-constexpr point<Rep> operator/(point<Rep> rhs, Rep2 value) {
-  return rhs /= value;
+constexpr point<Rep> operator/(point<Rep> lhs, Rep2 value) {
+  return lhs /= value;
 }
 
 
