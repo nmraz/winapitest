75fdfa63fb22ac4864df3cad781c3da59aa794fd
Noam Raz <noamraz8@gmail.com>
2017-10-17 18:01:17 +0300

    Declare explicit return type in base::run_task
    Intellisense has trouble with the deduced return type, and subsequently completely
    disables itself on any files which include the run_task header.

--- apptest/src/base/task_runner/run_task.h
+++ apptest/src/base/task_runner/run_task.h
@@ -7,11 +7,12 @@
 namespace base {
 
 template<typename Cb>
-auto run_task(std::shared_ptr<task_runner> runner, Cb&& callback) {
+auto run_task(std::shared_ptr<task_runner> runner, Cb&& callback)
+  -> future<typename impl::is_future<std::decay_t<std::invoke_result_t<Cb&&>>>::inner_type> {
   return make_future()
     .then([callback = std::forward<Cb>(callback)](auto&&) mutable {
-      return std::forward<Cb>(callback)();
-    }, std::move(runner));
+    return std::forward<Cb>(callback)();
+  }, std::move(runner));
 }
 
 }  // namespace base
