c1287f65eefb556e23daf81de47bd5c82069e201
Noam Raz <noamraz8@gmail.com>
2017-10-18 10:45:55 +0000

    Fix formatting in base::run_task

--- apptest/src/base/task_runner/run_task.h
+++ apptest/src/base/task_runner/run_task.h
@@ -11,8 +11,8 @@ auto run_task(std::shared_ptr<task_runner> runner, Cb&& callback)
   -> future<typename impl::is_future<std::decay_t<std::invoke_result_t<Cb&&>>>::inner_type> {
   return make_future()
     .then([callback = std::forward<Cb>(callback)](auto&&) mutable {
-    return std::forward<Cb>(callback)();
-  }, std::move(runner));
+      return std::forward<Cb>(callback)();
+    }, std::move(runner));
 }
 
 }  // namespace base
