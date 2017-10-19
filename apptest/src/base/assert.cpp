65594a59f6961344726d01ec6976184f34d85cdb
Noam Raz <noamraz8@gmail.com>
2017-10-19 20:37:01 +0300

    Call std::terminate when assertions fail
    This allows for faster termination when a debugger isn't attached, while still breaking
    when a debugger is attached.

--- apptest/src/base/assert.cpp
+++ apptest/src/base/assert.cpp
@@ -1,7 +1,7 @@
 #include "assert.h"
 
 #include "base/logging/logging.h"
-#include <intrin.h>
+#include <exception>
 
 namespace base::impl {
 
@@ -11,9 +11,12 @@ failed_assertion::failed_assertion(const char* file, int line, const char* cond,
   stream_ << "[" << func << "] Assertion `" << cond << "` failed: ";
 }
 
+#pragma warning(push)
+#pragma warning(disable: 4722)  // dtor never returns
 failed_assertion::~failed_assertion() {
   LOG_FULL(error, file_, line_) << stream_.str();
-  __debugbreak();
+  std::terminate();
 }
+#pragma warning(pop)
 
 }  // namespace base::impl
