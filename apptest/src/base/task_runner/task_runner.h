029b838e9718d08f4dd3cbc2eb9e8ebf8d0a6dc6
Noam Raz <noamraz8@gmail.com>
2017-11-04 16:02:04 +0200

    Accept cancellation tokens as first parameter to post_task
    This makes it easier to see that a cancellation token has been passed

--- apptest/src/base/task_runner/task_runner.h
+++ apptest/src/base/task_runner/task_runner.h
@@ -12,15 +12,15 @@ public:
 
   void post_task(task::callback_type callback);
   template<typename Cb>
-  void post_task(Cb&& callback, cancellation_token tok);
+  void post_task(cancellation_token tok, Cb&& callback);
 
   void post_task(task::callback_type callback, const task::delay_type& delay);
   template<typename Cb>
-  void post_task(Cb&& callback, const task::delay_type& delay, cancellation_token tok);
+  void post_task(cancellation_token tok, Cb&& callback, const task::delay_type& delay);
 
   void post_task(task::callback_type callback, const task::run_time_type& run_time);
   template<typename Cb>
-  void post_task(Cb&& callback, const task::run_time_type& run_time, cancellation_token tok);
+  void post_task(cancellation_token tok, Cb&& callback, const task::run_time_type& run_time);
 
 protected:
   virtual void do_post_task(task&& tsk) = 0;
@@ -28,7 +28,7 @@ protected:
 
 
 template<typename Cb>
-void task_runner::post_task(Cb&& callback, cancellation_token tok) {
+void task_runner::post_task(cancellation_token tok, Cb&& callback) {
   post_task([callback = std::forward<Cb>(callback), tok = std::move(tok)] {
     if (!tok.is_canceled()) {
       callback();
@@ -37,7 +37,7 @@ void task_runner::post_task(Cb&& callback, cancellation_token tok) {
 }
 
 template<typename Cb>
-void task_runner::post_task(Cb&& callback, const task::delay_type& delay, cancellation_token tok) {
+void task_runner::post_task(cancellation_token tok, Cb&& callback, const task::delay_type& delay) {
   post_task([callback = std::forward<Cb>(callback), tok = std::move(tok)] {
     if (!tok.is_canceled()) {
       callback();
@@ -46,7 +46,7 @@ void task_runner::post_task(Cb&& callback, const task::delay_type& delay, cancel
 }
 
 template<typename Cb>
-void task_runner::post_task(Cb&& callback, const task::run_time_type& run_time, cancellation_token tok) {
+void task_runner::post_task(cancellation_token tok, Cb&& callback, const task::run_time_type& run_time) {
   post_task([callback = std::forward<Cb>(callback), tok = std::move(tok)] {
     if (!tok.is_canceled()) {
       callback();
