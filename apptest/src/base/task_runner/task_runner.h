5f357919578ba10c20b6117d9b72d2288185d16b
Noam Raz <noamraz8@gmail.com>
2017-12-05 20:48:15 +0200

    Remove cancellation token overloads of task_runner::post_task
    This helps maintain the minimal interface necessary for task runners.

--- apptest/src/base/task_runner/task_runner.h
+++ apptest/src/base/task_runner/task_runner.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "base/non_copyable.h"
-#include "base/task_runner/cancellation.h"
 #include "base/task_runner/task.h"
 
 namespace base {
@@ -11,47 +10,11 @@ public:
   virtual ~task_runner() {}
 
   void post_task(task::callback_type callback);
-  template<typename Cb>
-  void post_task(cancellation_token tok, Cb&& callback);
-
   void post_task(task::callback_type callback, task::delay_type delay);
-  template<typename Cb>
-  void post_task(cancellation_token tok, Cb&& callback, task::delay_type delay);
-
   void post_task(task::callback_type callback, task::run_time_type run_time);
-  template<typename Cb>
-  void post_task(cancellation_token tok, Cb&& callback, task::run_time_type run_time);
 
 protected:
   virtual void do_post_task(task&& tsk) = 0;
 };
-
-
-template<typename Cb>
-void task_runner::post_task(cancellation_token tok, Cb&& callback) {
-  post_task([callback = std::forward<Cb>(callback), tok = std::move(tok)] {
-    if (!tok.is_canceled()) {
-      callback();
-    }
-  });
-}
-
-template<typename Cb>
-void task_runner::post_task(cancellation_token tok, Cb&& callback, task::delay_type delay) {
-  post_task([callback = std::forward<Cb>(callback), tok = std::move(tok)] {
-    if (!tok.is_canceled()) {
-      callback();
-    }
-  }, delay);
-}
-
-template<typename Cb>
-void task_runner::post_task(cancellation_token tok, Cb&& callback, task::run_time_type run_time) {
-  post_task([callback = std::forward<Cb>(callback), tok = std::move(tok)] {
-    if (!tok.is_canceled()) {
-      callback();
-    }
-  }, run_time);
-}
 
 }  // namespace base
