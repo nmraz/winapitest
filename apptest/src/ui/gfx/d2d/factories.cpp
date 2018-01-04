673d7b46e8622d7f0cb7b926005396d086067006
Noam Raz <noamraz8@gmail.com>
2018-01-04 19:30:04 +0200

    Make DirectX factory initialization thread-safe

--- apptest/src/ui/gfx/d2d/factories.cpp
+++ apptest/src/ui/gfx/d2d/factories.cpp
@@ -1,24 +1,31 @@
 #include "factories.h"
 
 #include "base/win/last_error.h"
+#include <mutex>
 
 namespace gfx::impl {
 
 const base::win::com_ptr<ID2D1Factory1>& get_d2d_factory() {
   static base::win::com_ptr<ID2D1Factory1> factory;
-  if (!factory) {
-    base::win::throw_if_failed(::D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, factory.addr()),
+  static std::once_flag guard;
+
+  std::call_once(guard, [&] {
+    base::win::throw_if_failed(::D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, factory.addr()),
       "Failed to create Direct2D factory");
-  }
+  });
+
   return factory;
 }
 
 const base::win::com_ptr<IDWriteFactory>& get_dwrite_factory() {
   static base::win::com_ptr<IDWriteFactory> factory;
-  if (!factory) {
+  static std::once_flag guard;
+
+  std::call_once(guard, [&] {
     base::win::throw_if_failed(::DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), factory.unknown_addr()),
       "Failed to create DirectWrite factory");
-  }
+  });
+
   return factory;
 }
 
