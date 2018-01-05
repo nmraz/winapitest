1b623985b64cdcfb01a321b4177e533e98571961
Noam Raz <noamraz8@gmail.com>
2018-01-06 01:04:41 +0200

    Fix gfx::lerp

--- apptest/src/ui/gfx/util.h
+++ apptest/src/ui/gfx/util.h
@@ -17,7 +17,7 @@ constexpr T three_pi_over_two = T(4.71238898038468985769396507491925432L);
 
 template<typename T>
 constexpr inline T lerp(T from, T to, double t) {
-  return static_cast<T>(from + (from - to) * t);
+  return static_cast<T>(from + (to - from) * t);
 }
 
 
