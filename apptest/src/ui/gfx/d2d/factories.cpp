08cab6f2f8b2af57969e14d12b5ac0401f28c055
Noam Raz <noamraz8@gmail.com>
2018-05-25 13:01:57 +0300

    Enable Direct2D debug messages in debug builds

--- apptest/src/ui/gfx/d2d/factories.cpp
+++ apptest/src/ui/gfx/d2d/factories.cpp
@@ -10,7 +10,13 @@ const base::win::com_ptr<ID2D1Factory1>& get_d2d_factory() {
   static std::once_flag guard;
 
   std::call_once(guard, [&] {
-    base::win::throw_if_failed(::D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, factory.addr()),
+    D2D1_FACTORY_OPTIONS opts = { D2D1_DEBUG_LEVEL_NONE };
+
+#ifndef NDEBUG
+    opts.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
+#endif
+
+    base::win::throw_if_failed(::D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, opts, factory.addr()),
       "Failed to create Direct2D factory");
   });
 
