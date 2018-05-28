8ee2e9cb294229fd1eee03288a9ea6973a490585
Noam Raz <noamraz8@gmail.com>
2018-05-28 10:52:12 +0300

    Implement base class for cached resources

---
+++ apptest/src/ui/gfx/resource/cached_resource.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace gfx {
+
+using resource_version = unsigned int;
+
+class cached_resource {
+public:
+  cached_resource(resource_version ver) : ver_(ver) {}
+  resource_version version() const { return ver_; }
+
+  virtual ~cached_resource() = 0 {}
+
+private:
+  const resource_version ver_;
+};
+
+}  // namespace gfx
