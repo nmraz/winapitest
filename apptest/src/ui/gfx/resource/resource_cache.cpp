74aacad2df627a6d05404180a53807bf3439f270
Noam Raz <noamraz8@gmail.com>
2018-05-30 10:55:13 +0300

    Implement basic resource cache

---
+++ apptest/src/ui/gfx/resource/resource_cache.cpp
@@ -0,0 +1,72 @@
+#include "resource_cache.h"
+
+namespace gfx {
+namespace {
+
+bool is_valid(resource_key* key, cached_resource* res) {
+  return key->version() == res->version();
+}
+
+}  // namespace
+
+void resource_cache::add(resource_key* key, std::unique_ptr<cached_resource> res) {
+  {
+    std::lock_guard hold(lock_);
+    entries_.insert_or_assign(key, std::move(res));
+  }
+  key->add_holder(this);
+}
+
+void resource_cache::remove(resource_key* key) {
+  std::lock_guard hold(lock_);
+
+  auto it = entries_.find(key);
+  if (it != entries_.end()) {
+    do_remove(it);
+  }
+}
+
+
+void resource_cache::clear() {
+  std::lock_guard hold(lock_);
+  for (auto it = entries_.begin(); it != entries_.end(); it = do_remove(it)) {}
+}
+
+void resource_cache::purge_invalid() {
+  std::lock_guard hold(lock_);
+
+  auto it = entries_.begin();
+  while (it != entries_.end()) {
+    const auto& [key, res] = *it;
+
+    if (!is_valid(key, res.get())) {
+      it = do_remove(it);
+    } else {
+      it++;
+    }
+  }
+}
+
+
+// PRIVATE
+
+cached_resource* resource_cache::do_find(resource_key* key) {
+  auto it = entries_.find(key);
+  if (it == entries_.end()) {
+    return nullptr;
+  }
+
+  cached_resource* res = it->second.get();
+  if (!is_valid(key, res)) {
+    do_remove(it);
+    return nullptr;
+  }
+  return res;
+}
+
+resource_cache::entry_iter resource_cache::do_remove(entry_iter it) {
+  it->first->remove_holder(this);
+  return entries_.erase(it);
+}
+
+}  // namespace gfx
---
+++ apptest/src/ui/gfx/resource/resource_cache.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ui/gfx/resource/cached_resource.h"
+#include "ui/gfx/resource/resource_key.h"
+#include <memory>
+#include <mutex>
+#include <unordered_map>
+
+namespace gfx {
+
+class resource_cache : private resource_key::holder {
+public:
+  ~resource_cache() { clear(); }
+
+  void add(resource_key* key, std::unique_ptr<cached_resource> res);
+  void remove(resource_key* key);
+
+  void clear();
+  void purge_invalid();
+
+  template<typename Res>
+  Res* find(resource_key* key);
+
+private:
+  using entry_map = std::unordered_map<resource_key*, std::unique_ptr<cached_resource>>;
+  using entry_iter = entry_map::iterator;
+
+  cached_resource* do_find(resource_key* key);
+  entry_iter do_remove(entry_iter it);
+
+  void key_destroyed(resource_key* key) override { remove(key); }
+
+  entry_map entries_;
+  std::mutex lock_;
+};
+
+
+template<typename Res>
+Res* resource_cache::find(resource_key* key) {
+  std::lock_guard hold(lock_);
+  return static_cast<Res*>(do_find(key));
+}
+
+}  // namespace gfx
