24c51a552688bc2645170b0d42ffc68ba642a06b
Noam Raz <noamraz8@gmail.com>
2018-05-30 13:00:13 +0300

    Add resource_cache::find_or_create
    This atomically looks up the resource, or creates it if it doesn't exist.

--- apptest/src/ui/gfx/resource/resource_cache.h
+++ apptest/src/ui/gfx/resource/resource_cache.h
@@ -4,6 +4,7 @@
 #include "ui/gfx/resource/resource_key.h"
 #include <memory>
 #include <mutex>
+#include <type_traits>
 #include <unordered_map>
 
 namespace gfx {
@@ -20,6 +21,9 @@ public:
 
   template<typename Res>
   Res* find(resource_key* key);
+
+  template<typename F>
+  auto find_or_create(resource_key* key, F&& factory);
 
 private:
   using entry_map = std::unordered_map<resource_key*, std::unique_ptr<cached_resource>>;
@@ -40,6 +44,23 @@ template<typename Res>
 Res* resource_cache::find(resource_key* key) {
   std::lock_guard hold(lock_);
   return static_cast<Res*>(do_find(key));
+}
+
+template<typename F>
+auto resource_cache::find_or_create(resource_key* key, F&& factory) {
+  using res_type = typename std::invoke_result_t<F>::element_type;
+  
+  std::lock_guard hold(lock_);
+
+  if (cached_resource* res = do_find(key)) {
+    return static_cast<res_type*>(res);
+  }
+
+  auto res_holder = std::forward<F>(f)();
+  res_type* res = res_holder.get();
+
+  do_add(key, std::move(res_holder));
+  return res;
 }
 
 }  // namespace gfx
