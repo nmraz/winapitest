0c8c97456c3d3ab24bc6a9cc175734f76f9f4439
Noam Raz <noamraz8@gmail.com>
2018-06-02 15:16:01 +0300

    Add gfx::device_image::is_valid

--- apptest/src/ui/gfx/image/device_image.h
+++ apptest/src/ui/gfx/image/device_image.h
@@ -10,6 +10,8 @@ public:
   device* device() const { return device_.get(); }
   device::ptr ref_device() const { return device_; }
 
+  bool is_valid() const { return !device()->is_lost(); }
+
 protected:
   device_image(device::ptr dev) : device_(std::move(dev)) {}
 
