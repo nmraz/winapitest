f4293cb84701f4cbc889a10482d65f6706a97fee
Noam Raz <noamraz8@gmail.com>
2018-06-02 14:45:41 +0300

    Simplify gfx::image interface

--- apptest/src/ui/gfx/image/image.h
+++ apptest/src/ui/gfx/image/image.h
@@ -1,18 +1,21 @@
 #pragma once
 
 #include "base/non_copyable.h"
+#include "base/win/com_ptr.h"
+#include "ui/gfx/device_impl.h"
+#include <d2d1_1.h>
 
 namespace gfx {
 namespace impl {
 
-struct image_impl;
+using d2d_image_ptr = base::win::com_ptr<ID2D1Image>;
 
 }  // namespace impl
 
 
 struct image : base::non_copy_movable {
   virtual ~image() {}
-  virtual const impl::image_impl* impl() const = 0;
+  virtual impl::d2d_image_ptr d2d_image(impl::device_impl* device) = 0;
 };
 
 }  // namespace gfx
