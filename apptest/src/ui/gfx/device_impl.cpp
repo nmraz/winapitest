7a8317a4699e6e25db3888fe75d6a67d9fa64a77
Noam Raz <noamraz8@gmail.com>
2018-06-03 23:41:28 +0300

    Add device_impl::create_bitmap

--- apptest/src/ui/gfx/device_impl.cpp
+++ apptest/src/ui/gfx/device_impl.cpp
@@ -1,6 +1,7 @@
 #include "device_impl.h"
 
 #include "base/win/last_error.h"
+#include "ui/gfx/d2d/convs.h"
 #include "ui/gfx/d2d/factories.h"
 
 namespace gfx::impl {
@@ -19,6 +20,7 @@ bool device_impl::is_lost() const {
   return FAILED(d3d_device_->GetDeviceRemovedReason());
 }
 
+
 d2d_dc_ptr device_impl::create_dc() {
   d2d_dc_ptr dc;
   base::win::throw_if_failed(
@@ -26,6 +28,32 @@ d2d_dc_ptr device_impl::create_dc() {
     "Failed to create Direct2D device context"
   );
   return dc;
+}
+
+d2d_bitmap_ptr device_impl::create_bitmap(const bitmap_info& info, const sizei& size,
+  D2D1_BITMAP_OPTIONS opts, base::span<const std::byte> data) {
+  auto props = D2D1::BitmapProperties1(
+    opts,
+    D2D1::PixelFormat(
+      static_cast<DXGI_FORMAT>(info.format()),
+      static_cast<D2D1_ALPHA_MODE>(info.alpha())
+    ),
+    info.dpix(),
+    info.dpiy()
+  );
+
+  d2d_bitmap_ptr bitmap;
+  base::win::throw_if_failed(
+    resource_dc()->CreateBitmap(
+      size_to_d2d_size(size),
+      data.data(),
+      static_cast<UINT32>(data.size() / size.height()),
+      props,
+      bitmap.addr()
+    ),
+    "Failed to create bitmap"
+  );
+  return bitmap;
 }
 
 }  // namespace gfx::impl
--- apptest/src/ui/gfx/device_impl.h
+++ apptest/src/ui/gfx/device_impl.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "base/span.h"
 #include "ui/gfx/device.h"
 #include "ui/gfx/d2d/resource_types.h"
+#include "ui/gfx/geom/size.h"
+#include "ui/gfx/image/bitmap_info.h"
 #include "ui/gfx/resource/resource_cache.h"
+#include <cstddef>
 
 namespace gfx::impl {
 
@@ -16,8 +20,10 @@ public:
   const d2d_device_ptr& d2d_device() { return d2d_device_; }
 
   d2d_dc_ptr create_dc();
-  const d2d_dc_ptr& resource_dc() { return resource_dc_; }
+  d2d_bitmap_ptr create_bitmap(const bitmap_info& info, const sizei& size, 
+    D2D1_BITMAP_OPTIONS opts = D2D1_BITMAP_OPTIONS_NONE, base::span<const std::byte> data = {});
 
+  const d2d_dc_ptr& resource_dc() { return resource_dc_; }
   resource_cache& cache() { return cache_; }
 
 private:
