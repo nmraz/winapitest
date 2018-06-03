9ea72af949497e868d6372d889244d78b947e750
Noam Raz <noamraz8@gmail.com>
2018-06-03 22:59:47 +0300

    Remove dependency of image.h on device_impl.h

--- apptest/src/ui/gfx/image/image.h
+++ apptest/src/ui/gfx/image/image.h
@@ -2,11 +2,12 @@
 
 #include "base/non_copyable.h"
 #include "base/win/com_ptr.h"
-#include "ui/gfx/device_impl.h"
 #include <d2d1_1.h>
 
 namespace gfx {
 namespace impl {
+
+class device_impl;
 
 using d2d_image_ptr = base::win::com_ptr<ID2D1Image>;
 
