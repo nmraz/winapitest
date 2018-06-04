a8095fbe9460ddbabf3b2a7b424e4591729b1f78
Noam Raz <noamraz8@gmail.com>
2018-06-04 21:44:39 +0300

    Implement size() and pixel_size() for gfx::device_bitmap

--- apptest/src/ui/gfx/image/device_bitmap.h
+++ apptest/src/ui/gfx/image/device_bitmap.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ui/gfx/d2d/resource_types.h"
+#include "ui/gfx/geom/size.h"
 #include "ui/gfx/image/bitmap_info.h"
 #include "ui/gfx/image/device_image.h"
 
@@ -9,8 +10,11 @@ namespace gfx {
 class device_bitmap : public device_image {
 public:
   const bitmap_info& info() const { return info_; }
-  const impl::d2d_bitmap_ptr& d2d_bitmap() const { return d2d_bitmap_; }
 
+  sizef size() const;
+  sizei pixel_size() const;
+
+  const impl::d2d_bitmap_ptr& d2d_bitmap() const { return d2d_bitmap_; }
   impl::d2d_image_ptr d2d_image(impl::device_impl*) override { return d2d_bitmap(); }
 
 protected:
