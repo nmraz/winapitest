d8011681d51a326892232918ab0a6ba29440d4d5
Noam Raz <noamraz8@gmail.com>
2018-06-16 20:06:24 +0300

    Implement missing gfx::mapped_texture ctor

--- apptest/src/ui/gfx/image/mapped_texture.cpp
+++ apptest/src/ui/gfx/image/mapped_texture.cpp
@@ -1,5 +1,6 @@
 #include "mapped_texture.h"
 
+#include "base/win/last_error.h"
 #include "ui/gfx/d2d/convs.h"
 
 namespace gfx {
@@ -20,6 +21,14 @@ sizei mapped_texture::pixel_size() const {
 
 base::span<const std::byte> mapped_texture::pixels() const {
   return { reinterpret_cast<const std::byte*>(mapped_.bits), pixel_size().height() * pitch() };
+}
+
+
+mapped_texture::mapped_texture(impl::d2d_bitmap_ptr mappable_bitmap, const bitmap_info& info)
+  : bitmap_(std::move(mappable_bitmap))
+  , info_(info) {
+  base::win::throw_if_failed(bitmap_->Map(D2D1_MAP_OPTIONS_READ, &mapped_),
+    "Failed to map GPU texture into memory");
 }
 
 }  // namespace gfx
