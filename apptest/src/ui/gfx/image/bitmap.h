f5fed173ae6d6dd94ab78c90da0b4a41c65637f3
Noam Raz <noamraz8@gmail.com>
2018-06-18 17:57:00 +0300

    Declare gfx::bitmap

--- apptest/src/ui/gfx/image/bitmap.h
+++ apptest/src/ui/gfx/image/bitmap.h
@@ -1,5 +1,31 @@
 #pragma once
 
+#include "base/span.h"
+#include "ui/gfx/geom/size.h"
+#include "ui/gfx/image/bitmap_info.h"
+#include "ui/gfx/image/image.h"
+#include <cstddef>
+#include <memory>
+#include <vector>
+
 namespace gfx {
+
+class bitmap : public image {
+public:
+  std::unique_ptr<bitmap> create(const bitmap_info& info, const sizei& size,
+    base::span<const std::byte> data);
+
+  const bitmap_info& info() const { return info_; }
+
+  sizef size() const;
+  sizei pixel_size() const;
+  int pitch() const;
+
+  base::span<const std::byte> pixels() const { return pixels_; }
+
+private:
+  std::vector<std::byte> pixels_;
+  bitmap_info info_;
+};
 
 }  // namespace gfx
