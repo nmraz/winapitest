36ea76e0da3d15eb621c9fb4ebe43bbdd721f2ab
Noam Raz <noamraz8@gmail.com>
2018-06-18 15:59:54 +0300

    Add bitmap pitch computation helper

--- apptest/src/ui/gfx/pixel_format.cpp
+++ apptest/src/ui/gfx/pixel_format.cpp
@@ -118,6 +118,16 @@ int bytes_per_pixel(pixel_format fmt) {
   return 0;
 }
 
+int compute_pitch(int total_size, int height, pixel_format fmt) {
+  ASSERT(total_size % height == 0) << "Invalid bitmap height";
+
+  if (!total_size) {
+    return 0;  // allow a height of 0 if there are no pixels
+  }
+
+  return total_size / height * bytes_per_pixel(fmt);
+}
+
 int pixel_offset(int x, int y, int pitch, pixel_format fmt) {
   return y * pitch + x * bytes_per_pixel(fmt);
 }
