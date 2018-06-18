a153353f26615d47c8ac7042a9f630795a097d1f
Noam Raz <noamraz8@gmail.com>
2018-06-18 19:05:44 +0300

    Implement basic gfx::bitmap_lock methods

--- apptest/src/ui/gfx/image/bitmap_lock.cpp
+++ apptest/src/ui/gfx/image/bitmap_lock.cpp
@@ -1,5 +1,24 @@
 #include "bitmap_lock.h"
 
+#include "ui/gfx/image/bitmap.h"
+
 namespace gfx {
+
+const bitmap_info& bitmap_lock::info() const {
+  return bmp_.info();
+}
+
+
+sizef bitmap_lock::size() const {
+  return bmp_.size();
+}
+
+sizei bitmap_lock::pixel_size() const {
+  return bmp_.pixel_size();
+}
+
+int bitmap_lock::pitch() const {
+  return bmp_.pitch();
+}
 
 }  // namespace gfx
