cc4f90cabcfece58144a65dbd4f4f59c89b34ba0
Noam Raz <noamraz8@gmail.com>
2018-06-18 15:06:04 +0300

    Simplify and correct channel reading and writing
    Previously, all of the channels were read into an integer and unpacked, which is wrong
    because x86 processors are little-endian. Now, the appropriate bytes are simply copied
    directly from memory.

--- apptest/src/ui/gfx/image/pixel_helpers.cpp
+++ apptest/src/ui/gfx/image/pixel_helpers.cpp
@@ -8,63 +8,31 @@
 namespace gfx {
 namespace {
 
-template<typename T>
-constexpr T max_val(std::size_t bits) {
-  return (T(1) << bits) - 1;
+float to_float(std::uint8_t val) {
+  return val / 255.f;
 }
 
-template<std::size_t I, std::size_t... Seq, typename... T>
-constexpr std::size_t partial_sum_after(T... vals) {
-  return ((Seq > I ? vals : 0) + ...);
-}
-
-
-template<std::size_t... Bits, typename P, typename... U, std::size_t... I>
-void unpack_helper(std::index_sequence<I...>, P packed, U&... unpacked) {
-  ((unpacked = static_cast<U>((packed >> partial_sum_after<I, I...>(Bits...)) & max_val<P>(Bits))), ...);
-}
-
-template<std::size_t... Bits, typename P, typename... U>
-void unpack(P packed, U&... unpacked) {
-  static_assert(sizeof...(Bits) == sizeof...(U));
-  unpack_helper<Bits...>(std::index_sequence_for<U...>{}, packed, unpacked...);
-}
-
-template<typename P, std::size_t... Bits, typename... U, std::size_t... I>
-P pack_helper(std::index_sequence<I...>, U... unpacked) {
-  return (((unpacked & max_val<P>(Bits)) << partial_sum_after<I, I...>(Bits...)) | ...);
-}
-
-template<typename P, std::size_t... Bits, typename... U>
-P pack(U... unpacked) {
-  static_assert(sizeof...(Bits) == sizeof...(U));
-  return pack_helper<P, Bits...>(std::index_sequence_for<U...>{}, unpacked...);
-}
-
-
-template<typename T, std::size_t Bits = sizeof(T) * CHAR_BIT>
-float to_float(T val) {
-  return static_cast<float>(val) / max_val<T>(Bits);
-}
-
-template<typename T, std::size_t Bits = sizeof(T) * CHAR_BIT>
-T from_float(float val) {
-  return static_cast<T>(val * max_val<T>(Bits));
+std::uint8_t from_float(float val) {
+  return static_cast<std::uint8_t>(val * 255);
 }
 
 
 void read_channels(const void* pixel, pixel_format fmt, float& r, float& g, float& b, float& a) {
-  std::uint32_t packed_pixel;
-  // casting here could violate strict aliasing, use memcpy instead
-  std::memcpy(&packed_pixel, pixel, sizeof(std::uint32_t));
+  const std::uint8_t* pixel8 = static_cast<const std::uint8_t*>(pixel);
 
   std::uint8_t r8 = 0, g8 = 0, b8 = 0, a8 = 0;
   switch (fmt) {
   case pixel_format::rgba8888:
-    unpack<8, 8, 8, 8>(packed_pixel, r8, g8, b8, a8);
+    r8 = *pixel8++;
+    g8 = *pixel8++;
+    b8 = *pixel8++;
+    a8 = *pixel8++;
     break;
   case pixel_format::bgra8888:
-    unpack<8, 8, 8, 8>(packed_pixel, b8, g8, r8, a8);
+    b8 = *pixel8++;
+    g8 = *pixel8++;
+    r8 = *pixel8++;
+    a8 = *pixel8++;
   default:
     NOTREACHED() << "Unknown pixel format";
   }
@@ -76,26 +44,29 @@ void read_channels(const void* pixel, pixel_format fmt, float& r, float& g, floa
 }
 
 void write_chanels(void* pixel, pixel_format fmt, float r, float g, float b, float a) {
-  auto r8 = from_float<std::uint8_t>(r);
-  auto g8 = from_float<std::uint8_t>(g);
-  auto b8 = from_float<std::uint8_t>(b);
-  auto a8 = from_float<std::uint8_t>(a);
+  std::uint8_t* pixel8 = static_cast<std::uint8_t*>(pixel);
 
-  std::uint32_t packed_pixel;
+  auto r8 = from_float(r);
+  auto g8 = from_float(g);
+  auto b8 = from_float(b);
+  auto a8 = from_float(a);
 
   switch (fmt) {
   case gfx::pixel_format::rgba8888:
-    packed_pixel = pack<std::uint32_t, 8, 8, 8, 8>(r8, g8, b8, a8);
+    *pixel8++ = r8;
+    *pixel8++ = g8;
+    *pixel8++ = b8;
+    *pixel8++ = a8;
     break;
   case gfx::pixel_format::bgra8888:
-    packed_pixel = pack<std::uint32_t, 8, 8, 8, 8>(b8, g8, r8, a8);
+    *pixel8++ = b8;
+    *pixel8++ = g8;
+    *pixel8++ = r8;
+    *pixel8++ = a8;
     break;
   default:
     NOTREACHED() << "Unknown pixel format";
   }
-
-  // casting here could violate strict aliasing, use memcpy instead
-  std::memcpy(pixel, &packed_pixel, sizeof(std::uint32_t));
 }
 
 
