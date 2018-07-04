d56af7c75bffe5527d6fdb1dd2089dbf98fc7266
Noam Raz <noamraz8@gmail.com>
2018-07-04 14:39:24 +0300

    Declare gfx::image_brush

--- apptest/src/ui/gfx/brush/image_brush.h
+++ apptest/src/ui/gfx/brush/image_brush.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#include "ui/gfx/brush/brush.h"
+#include "ui/gfx/image/image.h"
+
 namespace gfx {
+
+class image_brush : public brush {
+public:
+  static std::unique_ptr<image_brush> create(const image* img = nullptr);
+
+  const image* img() const;
+  void set_img(const image* img);
+
+private:
+  const image* img_;
+};
 
 }  // namespace gfx
