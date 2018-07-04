bffe96e7e75d07d37b0ae81def1adb8ce61e4508
Noam Raz <noamraz8@gmail.com>
2018-07-04 13:58:52 +0300

    Declare basic gfx::brush interface

--- apptest/src/ui/gfx/brush/brush.h
+++ apptest/src/ui/gfx/brush/brush.h
@@ -1,7 +1,19 @@
 #pragma once
 
+#include "base/non_copyable.h"
+#include "ui/gfx/matrix.h"
+
 namespace gfx {
 
+class brush : public base::non_copy_movable {
+public:
+  virtual ~brush() {}
 
+  const mat33f& transform() const;
+  void set_transform(const mat33f& tform);
+
+  float opacity() const;
+  void set_opacity(float opacity);
+};
 
 }  // namespace gfx
